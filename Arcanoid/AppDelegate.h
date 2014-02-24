d107a6772d9f7e941ff2fc582d6d4c6b371976fa
valeriyborisov <valeriyborisov9000xl@gmail.com>
2014-02-24 14:25:18 +0400

    Initial Commit

---
+++ Arcanoid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Arcanoid
+//
+//  Created by Валерий Борисов on 24.02.14.
+//  Copyright (c) 2014 Валерий Борисов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArcanoidTests/ArcanoidTests.m
@@ -0,0 +1,34 @@
+//
+//  ArcanoidTests.m
+//  ArcanoidTests
+//
+//  Created by Валерий Борисов on 24.02.14.
+//  Copyright (c) 2014 Валерий Борисов. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ArcanoidTests : XCTestCase
+
+@end
+
+@implementation ArcanoidTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
